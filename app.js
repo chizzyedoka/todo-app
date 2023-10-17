b677ff3b6b0928343dc78cf94ec37ebc7da01654
CHISOM EDOKA <edokachisom48@gmail.com>
2023-10-17 22:54:09 +0100

    ejs template

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });

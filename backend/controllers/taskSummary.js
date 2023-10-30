5e246ef18fe3a5d7ac38ad5c4848142be310a249
CHISOM EDOKA <edokachisom48@gmail.com>
2023-10-30 14:34:21 +0100

    remove redundant folders

--- backend/controllers/taskSummary.js
+++
@@ -1,4 +0,0 @@
-const completedTasks = 1;
-const pendingTasks = 2;
-const deletedTasks = 3;
-module.exports = { completedTasks, pendingTasks, deletedTasks };
--- backend/middleware/error.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (err, req, res, next) {
-  // Log the exception
-  console.log(err);
-  res.status(500).send("Server error");
-};
